1acf01de50ea5cd38cf7f507c173f49fd6366572
timadevelop <timadevelop@gmail.com>
2019-06-20 19:02:36 +0300

    lowecase-only tag names, random tag color

--- saasrest/tags/models.py
+++ saasrest/tags/models.py
@@ -2,11 +2,24 @@
 from django.db import models
 from colorfield.fields import ColorField
 
+import random
+
+TAG_COLORS = ['#40407a', '#706fd3', '#34ace0', '#227093',
+              '#218c74', '#ff5252', '#b33939', '#ff793f', '#ffb142', '#D6A2E8']
+
+
+def random_color():
+    return random.choice(TAG_COLORS)
+
 class Tag(models.Model):
     """Tag model"""
     name = models.TextField(max_length=20, blank=False,
                             null=False, unique=True)
-    color = ColorField(max_length=10, default='#686de0')
+    color = ColorField(max_length=10, default=random_color)
 
     def __str__(self):
         return 'Tag[id: {id}, name: {name}]'.format(id=self.id, name=self.name)
+
+    def save(self, *args, **kwargs):
+        self.name = self.name.lower()
+        return super(Tag, self).save(*args, **kwargs)
