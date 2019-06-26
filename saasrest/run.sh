0075b395de4142b51da7d884707babe8f477811f
timadevelop <timadevelop@gmail.com>
2019-06-26 21:56:41 +0300

    use daphne

--- saasrest/run.sh
+++ saasrest/run.sh
@@ -2,23 +2,10 @@
 
 pip install -r requirements.txt
 python manage.py compilemessages -l bg
-# python manage.py makemigrations saas_core
-# python manage.py makemigrations authentication
-# python manage.py makemigrations categories
-# python manage.py makemigrations locations
-# python manage.py makemigrations feedback
-# python manage.py makemigrations messaging
-# python manage.py makemigrations notifications
-# python manage.py makemigrations payments
-# python manage.py makemigrations public_configs
-# python manage.py makemigrations services
-# python manage.py makemigrations tags
-# python manage.py makemigrations votes
-# python manage.py makemigrations feed
+python manage.py makemigrations saas_core authentication categories locations feedback messaging notifications payments public_configs services tags votes feed
 python manage.py makemigrations
 
 python manage.py migrate
-
 
 if [ "$SAAS_ENV" = "production" ]; then
     echo Production env
@@ -26,5 +13,6 @@ if [ "$SAAS_ENV" = "production" ]; then
     python manage.py runserver "0.0.0.0:${PORT}"
 else
     echo Developement env
-    python manage.py runserver "0.0.0.0:${PORT}"
+    daphne -b 0.0.0.0 -p ${PORT} saasrest.asgi:application
+    # python manage.py runserver "0.0.0.0:${PORT}"
 fi
