ae246aeec84408d3b8afaa8410c1392c37fa1515
timadevelop <timadevelop@gmail.com>
2019-11-01 12:59:12 +0200

    fix run.sh

--- saasrest/run.sh
+++ saasrest/run.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-# pip install -r requirements.txt
-# python manage.py compilemessages -l bg
-# python manage.py makemigrations saas_core authentication categories locations feedback messaging notifications payments public_configs services tags votes feed
-# python manage.py makemigrations
+pip install -r requirements.txt
+python manage.py compilemessages -l bg
+python manage.py makemigrations saas_core authentication categories locations feedback messaging notifications payments public_configs services tags votes feed
+python manage.py makemigrations
 
-# python manage.py migrate
+python manage.py migrate
 
 if [ "$SAAS_ENV" = "production" ]; then
     echo Production env
-    #python manage.py runsslserver --certificate /etc/ssl/certs/fullchain.crt --key /etc/ssl/certs/fullchain.key "0.0.0.0:${PORT}"
-    python manage.py runserver "0.0.0.0:${PORT}"
+    # daphne -b 0.0.0.0 -p ${PORT} saasrest.asgi:application
 else
     echo Developement env
-    echo "Python no daphne"
     # daphne -b 0.0.0.0 -p ${PORT} saasrest.asgi:application
     python manage.py runserver "0.0.0.0:${PORT}"
 fi
